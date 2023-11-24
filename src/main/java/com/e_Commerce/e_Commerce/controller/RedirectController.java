44b20c1b78dba9cffc9f11895b57eb30b4a7474b
Hanabi-TheFox <pintoethan@cy-tech.fr>
2023-11-24 16:11:43 +0100

    page renommé + RedirectionController fait

--- src/main/java/com/e_Commerce/e_Commerce/controller/RedirectController.java
+++ src/main/java/com/e_Commerce/e_Commerce/controller/RedirectController.java
@@ -7,13 +7,41 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class RedirectController {
     @GetMapping("/")
-    public String index(ModelMap model) {
-        return "index";
-    }
+    public String index(ModelMap model) { return "index";}
     @GetMapping("/Ajouter_Moderateur")
     public String ajouterModerateur(ModelMap model) { return "pageAjouterModerateur";}
-    @GetMapping("/connexion")
-    public String connexion(ModelMap model) {
-        return "pageConnexion";
-    }
+    @GetMapping("/Ajouter_Moyen_De_Paiement")
+    public String ajouterMoyenPaiement(ModelMap model) { return "pageAjouterMoyenPaiement";}
+    @GetMapping("/Ajouter_Produit")
+    public String ajouterProduit(ModelMap model) { return "pageAjouterProduit";}
+    @GetMapping("/Ajouter_Solde")
+    public String ajouterSolde(ModelMap model) { return "pageAjouterSolde";}
+    @GetMapping("/Changer_Mot_De_Passe")
+    public String changerMotDePasse(ModelMap model) { return "pageChangerMotDePasse";}
+    @GetMapping("/Confirmer_Paiement")
+    public String confirmerPaiement(ModelMap model) { return "pageConfirmerPaiement";}
+    @GetMapping("/Connexion")
+    public String connexion(ModelMap model) { return "pageConnexion";}
+    @GetMapping("/Convertir_Points")
+    public String convertirPoints(ModelMap model) { return "pageConvertPoints";}
+    @GetMapping("/Inscription")
+    public String inscription(ModelMap model) { return "pageInscription";}
+    @GetMapping("/Liste_Moderateurs")
+    public String listeModerateurs(ModelMap model) { return "pageListeModerateurs";}
+    @GetMapping("/Modifier_Droits_Moderateur")
+    public String modifierDroitsModerateur(ModelMap model) { return "pageModifierDroitsModerateur";}
+    @GetMapping("/Modifier_Produit")
+    public String modifierProduit(ModelMap model) { return "pageModifierProduit";}
+    @GetMapping("/Modifier_Profil")
+    public String modifierProfil(ModelMap model) { return "pageModifierProfil";}
+    @GetMapping("/Panier")
+    public String panier(ModelMap model) { return "pagePanier";}
+    @GetMapping("/Produit")
+    public String produit(ModelMap model) { return "pageProduit";}
+    @GetMapping("/Produits")
+    public String produits(ModelMap model) { return "pageProduits";}
+    @GetMapping("/Profil")
+    public String profil(ModelMap model) { return "pageProfil";}
+    @GetMapping("/Supprimer_Moderateur")
+    public String supprimerModerateur(ModelMap model) { return "pageSupprimerModerateur";}
 }
