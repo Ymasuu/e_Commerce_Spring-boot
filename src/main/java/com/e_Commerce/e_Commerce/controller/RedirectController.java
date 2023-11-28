56ad71577b25cc15bcd639d687f5fbd19bdfafbe
rdnatos <rdardiles123@gmail.com>
2023-11-28 12:11:32 +0100

    Modifier Profil

--- src/main/java/com/e_Commerce/e_Commerce/controller/RedirectController.java
+++ src/main/java/com/e_Commerce/e_Commerce/controller/RedirectController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.math.BigDecimal;
 import java.util.Optional;
@@ -103,10 +104,60 @@ public class RedirectController {
     public String modifierDroitsModerateur(ModelMap model) { return "pageModifierDroitsModerateur";}
     @GetMapping("/Modifier_Produit")
     public String modifierProduit(ModelMap model) { return "pageModifierProduit";}
+
+    ///PAGE MODIFIER PROFIL /////////////////////////////////////////////////////////////////////
     @GetMapping("/Modifier_Profil")
-    public String modifierProfil(ModelMap model) { return "pageModifierProfil";}
+    public String modifierProfil(ModelMap model, HttpSession session) {
+        Utilisateur user = (Utilisateur) session.getAttribute("user");
+        Client client = (Client) session.getAttribute("client");
+        Moderateur moderateur = (Moderateur) session.getAttribute("moderateur");
+        //Admin admin  = (Admin) session.getAttribute("admin");
+        model.addAttribute("user", user);
+        model.addAttribute("client", client);
+        System.out.println("ERROR MESSAGE : " + model.getAttribute("errorMessage"));
+        return "pageModifierProfil";}
+
+    @PostMapping("/Modifier_Profil")
+    public String modifierProfilPost(RedirectAttributes redirectAttributes,
+                                     ModelMap model, HttpSession session, @RequestParam ("nom") String nom,
+                                     @RequestParam ("prenom") String prenom, @RequestParam ("mail") String email,
+                                     @RequestParam ("mdp") String mdp) {
+
+        // Il est verifiée si le mot de passe est correct
+        Utilisateur user = (Utilisateur) session.getAttribute("user");
+        if (user.getMotDePasse().equals(mdp)) {
+            // Le mot de passe est correct, on modifie les données de l'utilisateur
+            user.setNom(nom);
+            user.setPrenom(prenom);
+            user.setMail(email);
+            user.setMotDePasse(mdp);
+            //TODO update d'utilisateur dans la bdd
+            utilisateurService.saveUser(user);
+            session.setAttribute("user", user);
+            session.setAttribute("client", client);
+            model.addAttribute("user",user);
+            model.addAttribute("client",client);
+            return "redirect:/Profil";
+        } else {
+            // Le mot de passe est incorrect, on affiche un message d'erreur
+            String errorMessage = "Mot de passe incorrect";
+            //Permet de redirectionner le message sans avoir besoin du model
+            //vers un autre mapping de type Get.
+            //Cela permet aussi de ne pas avoir le besoin de retourner une page.
+            //Il est possible donc de retourner un GetMapping, ce qu'est sembable
+            //à la logique des servlettes
+            redirectAttributes.addFlashAttribute("errorMessage", errorMessage);
+            return "redirect:/Modifier_Profil";
+        }
+
+
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////
     @GetMapping("/Panier")
     public String panier(ModelMap model) { return "pagePanier";}
+
+    ///PAGE PROFIL ///////////////////////////////////////////////////////////////////
 
     @GetMapping("/Profil")
     public String utilisateur(ModelMap model, HttpSession session) {
@@ -119,6 +170,9 @@ public class RedirectController {
         model.addAttribute("client", client);
         return "pageProfil";
     }
+
+
+    //////////////////////////////////////////////////////////////////////////////////
 
     @GetMapping("/Supprimer_Moderateur")
     public String supprimerModerateur(ModelMap model) { return "pageSupprimerModerateur";}
