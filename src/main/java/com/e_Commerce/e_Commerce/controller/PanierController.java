d86ff6f39b353e54d0390042bab56739a9232db0
Ymasuu <samy.belbouab95@gmail.com>
2023-11-29 18:50:20 +0100

    Modifier quantité produit dans panier (manque plus qu'à mettre un maximum pour la nouvelle quantité)

--- src/main/java/com/e_Commerce/e_Commerce/controller/PanierController.java
+++ src/main/java/com/e_Commerce/e_Commerce/controller/PanierController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -28,28 +29,52 @@ public class PanierController {
 
 
     @GetMapping("/Panier")
-    public String panier(ModelMap model) { return "pagePanier.html";}
+    public String panier(ModelMap model) {
+        model.addAttribute("produits", produitsService.getProduct());
+        return "pagePanier";
+    }
 
     @PostMapping("/Panier")
     public String panierPost(ModelMap model, HttpSession session,
-         @RequestParam String action, @RequestParam (required = false) Integer produitId) {
+         @RequestParam String action, @RequestParam (required = false) Integer produitId, @RequestParam (required = false) Integer produitQuantite) {
         if (action.equals("vider")){
             Client client = (Client) session.getAttribute("client");
             Commande panierVide = new Commande(client.getIdClient(), 0);
             session.setAttribute("panier", panierVide);
-        }
-        if (action.equals("supprimer")){
+        }else {
             Commande newPanier = (Commande) session.getAttribute("panier");
-            for (Produit p : newPanier.getPanier()){
-                if (p.getIdProduit() == produitId){
-                    newPanier.setPrix(newPanier.getPrix() - (p.getPrix() * p.getStock()));
-                    newPanier.getPanier().remove(p);
-                    newPanier.majNbrProduit();
-                    session.setAttribute("panier", newPanier);
-                    break;
+            if (action.equals("supprimer")){
+                for (Produit p : newPanier.getPanier()){
+                    if (p.getIdProduit() == produitId){
+                        newPanier.setPrix(newPanier.getPrix() - (p.getPrix() * p.getStock()));
+                        newPanier.getPanier().remove(p);
+                        newPanier.majNbrProduit();
+                        session.setAttribute("panier", newPanier);
+                        break;
+                    }
+                }
+            }else { // action.equals("modifier")
+                for (Produit p : newPanier.getPanier()){
+                    if (p.getIdProduit() == produitId){
+                        newPanier.setPrix(newPanier.getPrix() - (p.getPrix() * p.getStock()));
+                        p.setStock(produitQuantite);
+                        newPanier.setPrix(newPanier.getPrix() + (p.getPrix() * p.getStock()));
+                        session.setAttribute("panier", newPanier);
+                        break;
+                    }
                 }
             }
         }
         return "redirect:/Panier";
     }
+
+/*    @ModelAttribute("stockProduit")
+    public Produit getProduitById(@RequestParam int idProduit) {
+        for (Produit produit : produitsService.getProduct()) {
+            if (produit.getIdProduit() == idProduit) {
+                return produit;
+            }
+        }
+        return null;
+    }*/
 }
