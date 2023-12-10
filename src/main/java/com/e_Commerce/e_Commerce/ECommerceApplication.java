0da9e7c7e434472ba8bf56b6c0bca8f1d1273a9b
Ymasuu <samy.belbouab95@gmail.com>
2023-12-10 14:29:59 +0100

    adjustment code structure

--- src/main/java/com/e_Commerce/e_Commerce/ECommerceApplication.java
+++ src/main/java/com/e_Commerce/e_Commerce/ECommerceApplication.java
@@ -1,32 +1,23 @@
 package com.e_Commerce.e_Commerce;
 
-import com.e_Commerce.e_Commerce.model.entity.Produit;
 import com.e_Commerce.e_Commerce.service.ProduitsService;
-import org.hibernate.sql.ast.tree.expression.Over;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.ui.ModelMap;
-
-import java.util.Optional;
 
 @SpringBootApplication
 public class ECommerceApplication implements CommandLineRunner {
 
-	@Autowired
-	private ProduitsService produitsService;
+    @Autowired
+    private ProduitsService produitsService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(ECommerceApplication.class, args);
-	}
-	@Override
-	public void run(String... args) throws Exception{
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+    }
 
 }
